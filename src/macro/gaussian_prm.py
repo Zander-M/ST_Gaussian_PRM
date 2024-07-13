40664921a8fb737c89013349b48f623b019da907
Zander <zm800@nyu.edu>
2024-07-13 12:38:05 -0700

    update prm

--- src/macro/gaussian_prm.py
+++ src/macro/gaussian_prm.py
@@ -1,41 +1,72 @@
 """
     Gaussian PRM based on map info.
 """
+import random
+from collections import namedtuple
+
 import numpy as np
 from scipy.stats import gaussian_kde
 
-class GaussianNode:
-    """
-        Gaussian Node
-    """
-    def __init__(self) -> None:
-        pass
+Point = namedtuple("Point", ["x", "y"])
 
-    def get_capacity(self):
-        pass
-
-    def get_gaussian(self):
-        pass
     
 class GaussianPRM:
     """
         Gaussian PRM
     """
-    def __init__(self) -> None:
+
+    class GaussianNode:
+        """
+            Gaussian Node
+        """
+        def __init__(self, pos, cvar_thresh) -> None:
+            self.pos = pos
+            self.cvar_thresh = cvar_thresh
+
+        def compute_distribution(self):
+            """
+                Compute the current distribution based on distance to closest obstacle
+            """
+
+        def get_CVaR(self):
+            """
+                Return CVaR value
+            """
+            return self.cvar_thresh
+
+        def get_capacity(self, density):
+            """
+                Return capacity of the node
+            """
+            return 
+            pass
+
+        def get_gaussian(self):
+            pass
+
+    def __init__(self, map_size, obstacles) -> None:
+        self.map_size = map_size
+        self.obstacles = obstacles
+        self.abstract_prm = None
+        self.gaussian_prm = None
+        pass
+    
+    def point_collision(self, point):
+        pass
+        
+    def edge_collision(self, p1, p2):
         pass
 
-    def build_gaussian_prm(self):
+    def sample_free_space(self)
+
+    def build_prm(self):
         """
-            Build Gaussian PRM
+            Build Gaussian PRM. Construct regular PRM, then convert the 
+            nodes to Gaussian Nodes.
         """
+
         pass
 
-    def abstract_prm(self):
-        """
-            Construct abstract PRM for path search. Node locations are the center
-            of the Gaussian distributions.
-        """
-        pass
 
     def astar_search(self, start, goal):
         """
