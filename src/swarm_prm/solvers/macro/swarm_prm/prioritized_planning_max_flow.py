cfbba02f1a8f81f1c1de0c89a13547dd4fb37b12
Zander Mao <zma40@sfu.ca>
2024-11-22 00:21:23 -0800

    pp updated

--- src/swarm_prm/solvers/macro/swarm_prm/prioritized_planning_max_flow.py
+++ src/swarm_prm/solvers/macro/swarm_prm/prioritized_planning_max_flow.py
@@ -15,74 +15,6 @@ from matplotlib import pyplot as plt
 import networkx as nx
 from swarm_prm.solvers.macro.swarm_prm.gaussian_prm import GaussianPRM
 
-class ST_AStar:
-    """
-        Single agent spatio-temporal A star search on PRM for max flow w.r.t.
-        the constriants
-
-        We assume agents are always planning from super source to super sink
-
-    """
-
-    def __init__(self, nodes, graph, constraints):
-        """
-            nodes:
-                Node indexes
-            
-            graph: 
-                Abstract Graph
-
-
-            constraints:
-                Constriants indexed by timestep.
-                Format:
-                {timestep:[(v1, v2), ...], ...}
-        """
-        self.nodes = nodes
-        self.graph = graph
-        self.constraints = constraints
-
-    def a_star(self):
-        """
-            A Star search
-        """
-        timestep = 0
-        open_heap = []
-        node_idx = -1
-        f_value = timestep + self.graph.get_heuristic(node_idx)
-        heapq.heappush(open_heap, (f_value, timestep, node_idx))
-        while open_heap:
-            _, timestep, node_idx =heapq.heappop(open_heap)
-            timestep += 1
-            next_nodes = self.graph.get_neighbors(node_idx) + [node_idx]
-            actions = [[node_idx, next_node] for next_node in next_nodes]
-            constrained_actions = self.apply_constriants(actions, timestep)
-
-            pass
-    
-    def apply_constriants(self, actions, timestep):
-        """
-            return valid actions that respects the constriants
-
-            actions: [(v1, v2), ...] 
-                Travel from v1 to v2
-            
-            timestep:
-                Timestep of of the action
-        """
-        t_constriants = self.get_constraint_at_timestep(timestep)
-        return [action for action in actions if action[1] not in t_constriants[action[0]]]
-
-    
-    def get_constraint_at_timestep(self, timestep):
-        """
-            return constriants indexed by node idx at timestep t
-        """
-        t_constraints = defaultdict(list)
-        for constraint in self.constraints[timestep]:
-            t_constraints[constraint[0]].append(constraint[1])
-        return t_constraints
-
 class AbstractGraph:
     """
         Abstract Graph for A star search
@@ -95,28 +27,16 @@ class AbstractGraph:
         self.nodes_idx = [i for i in range(len(self.nodes))]
 
         self.nodes = self.gaussian_prm.samples 
-        self.graph = self.build_capacity_graph()
+        self.graph = self.build_graph()
         self.starts_idx = self.gaussian_prm.start_idx
         self.goals_idx = self.gaussian_prm.start_idx
         self.heuristic = self.compute_heuristic()
 
-    def build_capacity_graph(self, method="MIN_CAPACITY"):
+    def build_graph(self, method="MIN_CAPACITY"):
         """
             Build Graph with capacity constraints
         """
         graph = defaultdict(list)
-        ss = "SS" # super start
-        sg = "SG" # super goal
-
-        # Add super start/goal
-
-        for start_idx in self.gaussian_prm.starts_idx:
-            capacity = self.gaussian_prm.gaussian_nodes[start_idx].get_capacity(self.agent_radius)
-            graph[ss].append((start_idx, capacity))
-
-        for goal_idx in self.gaussian_prm.goals_idx:
-            capacity = self.gaussian_prm.gaussian_nodes[goal_idx].get_capacity(self.agent_radius)
-            graph[goal_idx].append((sg, capacity))
 
         if method == "MIN_CAPACITY":
             for edge in self.gaussian_prm.roadmap:
@@ -129,6 +49,12 @@ class AbstractGraph:
         elif method == "VERTEX_CAPACITY":
             assert False, "Unimplemented roadmap graph construction method."
         return graph
+    
+    def get_starts(self):
+        """
+            Return start nodes
+        """
+        return self.starts_idx
 
     def update_capacity(self, constraints):
         """
@@ -140,10 +66,6 @@ class AbstractGraph:
             Get path flow based on map
         """
 
-    def build_graph(self):
-        """
-            Build abstract graph from Gaussian PRM
-        """
     
     def get_neighbors(self, node_idx):
         """
@@ -170,6 +92,113 @@ class AbstractGraph:
         """
         return self.heuristic[node_idx]
 
+class ST_AStar:
+    """
+        Single agent spatio-temporal A star search on PRM for max flow w.r.t.
+        the constriants
+
+        We assume agents are always planning from super source to super sink
+
+    """
+
+    def __init__(self, nodes, graph:AbstractGraph, constraints):
+        """
+            nodes:
+                Node indexes
+            
+            graph: 
+                Abstract Graph
+
+            constraints:
+                Constriants indexed by timestep.
+                Format:
+                {timestep:[(v1, v2), ...], ...}
+        """
+        self.nodes = nodes
+        self.graph = graph
+        self.constraints = constraints
+        self.ss = len(self.graph.nodes)
+        self.sg = len(self.graph.nodes) + 1
+
+    def a_star(self):
+        """
+            A Star search
+        """
+        open_heap = []
+
+        # Add starts
+        actions = []
+        for start_idx in self.graph.starts_idx:
+            actions.append([None, start_idx])
+
+        constrained_actions = self.apply_constriants(actions, 0)
+        for _, start_idx in constrained_actions:
+            curr_state_dict = {
+                "t": 0,
+                "parent": None,
+                "node_idx": start_idx
+            }
+            f_value = self.graph.get_heuristic(start_idx)
+            heapq.heappush(open_heap, (f_value, curr_state_dict))
+
+        while open_heap:
+            # pop from open list, take one step and add new node to open list
+            _, curr_state_dict =heapq.heappop(open_heap)
+            curr_node_idx = curr_state_dict["node_idx"]
+
+            # if reaching one of the goals, 
+            if self.goal_test(curr_node_idx):
+                break
+
+            # all neighboring nodes + wait
+            next_nodes = self.graph.get_neighbors(curr_node_idx) + [curr_node_idx]
+            actions = [[curr_node_idx, next_node] for next_node in next_nodes]
+            constrained_actions = self.apply_constriants(actions, timestep)
+            for action in constrained_actions:
+                state_dict = {
+                    "t" : curr_state_dict["t"] + 1,
+                    "parent" : curr_state_dict,
+                    "node_idx" : action[1]
+                }
+                f_value = curr_state_dict["t"] + self.graph.get_heuristic(action[1])
+                heapq.heappush(open_heap, (f_value, state_dict))
+        
+        # Construct path based on trajectory
+        path = []
+        while curr_state_dict["parent"] is not None:
+            path.append(curr_state_dict["node_idx"])
+            curr_state_dict = curr_state_dict["parent"]
+        return path
+    
+    def apply_constriants(self, actions, timestep):
+        """
+            return valid actions that respects the constriants
+
+            actions: [(v1, v2), ...] 
+                Travel from v1 to v2
+            
+            timestep:
+                Timestep of of the action
+        """
+        t_constriants = self.get_constraint_at_timestep(timestep)
+        return [action for action in actions if action[1] not in t_constriants[action[0]]]
+    
+    def get_constraint_at_timestep(self, timestep):
+        """
+            return constriants indexed by node idx at timestep t
+        """
+        t_constraints = defaultdict(list)
+        for constraint in self.constraints[timestep]:
+            t_constraints[constraint[0]].append(constraint[1])
+        return t_constraints
+
+    def goal_test(self, node_idx):
+        """
+            Test if goal is reached
+        """
+        return node_idx in self.graph.goals_idx
+
+
 
 
 class PrioritizedPlanning_MaxFlow:
@@ -182,58 +211,3 @@ class PrioritizedPlanning_MaxFlow:
         self.target_flow = target_flow
         self.abstract_graph= AbstractGraph(gaussian_prm, agent_radius)
         self.nodes = [i for i in range(len(self.gaussian_prm.samples))]
-
-
-    def find_earliest_timestep(self):
-        """
-            Find earliest timestep such that the graph reaches target flow
-        """
-        timestep = 0
-        max_flow = 0
-        flow_dict = {}
-        while timestep < self.max_timestep:
-            super_source, super_sink, teg, restricted_edges = self.build_teg(timestep)
-            max_flow, flow_dict = nx.maximum_flow(teg, super_source, super_sink)
-            print("timestep:", timestep, "max_flow:", max_flow)
-            if max_flow == self.target_flow:
-                return max_flow, flow_dict, timestep, teg, restricted_edges
-            else:
-                timestep += 1
-
-        return None, None, None, None, None
-    
-    def flow_to_trajectory(self, flow_dict):
-        """
-            Convert Flow to Trajectories per agent
-        """
-        trajectory = []
-        return trajectory
-
-    def visualize_teg(self, teg, restricted_edges):
-        """
-            Visualize TEG 
-        """
-        node_labels = {node: node for node in teg.nodes()}
-        edge_labels = nx.get_edge_attributes(teg, 'capacity')
-
-        # Draw the graph
-        plt.figure(figsize=(10, 8))  # Set the size of the figure
-        teg = teg.to_undirected()
-        pos = nx.bfs_layout(teg, "VS")  # type: ignore # Compute positions using the spring layout
-        pos["SS"] = pos["VS"]
-
-        # Hide visualization source and extra edges
-        teg = nx.restricted_view(teg, ["VS"], restricted_edges)
-
-
-        # Draw nodes and edges
-        nx.draw(teg, pos, with_labels=True, node_color='lightblue', node_size=300, font_size=16, edge_color='gray')
-        nx.draw_networkx_edge_labels(teg, pos, edge_labels=edge_labels)
-
-
-        # Display the plot
-        plt.title("Graph Visualization")
-        plt.xlabel("X Coordinate")
-        plt.ylabel("Y Coordinate")
-        plt.grid(True)
-        plt.show()
