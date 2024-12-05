2114d0b06b2409a64d4de7e7878b62f4a121e16d
Zander <zm800@nyu.edu>
2024-12-05 14:28:40 -0800

    add bidirectional bfs

--- src/swarm_prm/solvers/macro/swarm_teg/max_flow.py
+++ src/swarm_prm/solvers/macro/swarm_teg/max_flow.py
@@ -31,24 +31,90 @@ class MaxFlowSolver:
                 augment_graph[v][u] = 0
         return augment_graph
         
-    def bfs(self, start, goal):
+    def bfs(self):
         """
             BFS for finding augmenting path
         """
-        prev = {start: None}
-        flow = {start: float('inf')}
-        open_list = deque([start])
+        prev = {self.start: None}
+        flow = {self.start: float('inf')}
+        open_list = deque([self.start])
 
         while open_list:
             curr_node = open_list.popleft()
-            if curr_node == goal:
-                return prev, flow[goal]
+
             for neighbor, capacity in self.residual_graph[curr_node].items():
                 if neighbor not in prev and capacity > 0:
                     prev[neighbor] = curr_node
                     flow[neighbor] = min(flow[curr_node], capacity)
-                    open_list.append(neighbor)
+
+                if neighbor == self.goal:
+                   return prev, flow[self.goal]
+
+                open_list.append(neighbor)
         return None, 0
+
+    def bidirectional_bfs(self):
+        """
+            Bidirectional BFS
+        """
+        forward_queue = deque([self.start])
+        backward_queue= deque([self.goal])
+        forward_parent= {self.start: None}
+        backward_parent= {self.goal: None}
+        forward_visited = set([self.start])
+        backward_visited= set([self.goal ])
+
+        while forward_queue and backward_queue:
+            if forward_queue:
+                current = forward_queue.popleft()
+                for neighbor, capacity in self.residual_graph[current].items():
+                    if neighbor not in forward_parent and capacity > 0:
+                        forward_parent[neighbor] = current
+                        forward_visited.add(neighbor)
+                        forward_queue.append(neighbor)
+                        if neighbor in backward_visited:
+                            return self._construct_path(forward_parent, backward_parent, neighbor)
+
+            if backward_queue:
+                current = backward_queue.popleft()
+                for neighbor, capacity in self.residual_graph[current].items():
+                    if current not in backward_parent and capacity > 0:
+                        backward_parent[current] = neighbor
+                        backward_visited.add(neighbor)
+                        backward_queue.append(neighbor)
+                        if neighbor in forward_visited:
+                            return self._construct_path(forward_parent, backward_parent, neighbor)
+        return None, 0
+
+    def _construct_path(self, forward_parent, backward_parent, meeting_point):
+        """
+        Construct the full path from source to sink using the meeting point.
+        """
+        path = []
+        flow = float("inf")
+
+        # Build forward path
+        current = meeting_point
+        while current is not None:
+            path.append(current)
+            if forward_parent[current] is not None:
+                flow = min(flow, self.residual_graph[forward_parent[current]][current])
+            current = forward_parent[current]
+
+        path = path[::-1]  # Reverse to get source to meeting point
+
+        # Build backward path
+        current = meeting_point
+        while current is not None:
+            if backward_parent[current] is not None:
+                flow = min(flow, self.residual_graph[current][backward_parent[current]])
+            current = backward_parent[current]
+            if current is not None:
+                path.append(current)
+
+        return path, flow
+
+    
 
     def update_flow(self, path, flow):
         """
@@ -69,15 +135,13 @@ class MaxFlowSolver:
             total_flow = 0
 
             while True:
-                prev, flow = self.bfs(self.start, self.goal)
-                if not prev:
+                path, flow = self.bidirectional_bfs()
+                if not path:
                     break
-                node = self.goal
-                while node != self.start:
-                    prev_node = prev[node]
-                    self.residual_graph[prev_node][node] -= flow
-                    self.residual_graph[node][prev_node] += flow
-                    node = prev[node]
+                
+                for u, v in zip(path[:-1], path[1:]):
+                    self.residual_graph[u][v] -= flow
+                    self.residual_graph[v][u] += flow
                 total_flow += flow
 
             return total_flow, self.residual_graph
