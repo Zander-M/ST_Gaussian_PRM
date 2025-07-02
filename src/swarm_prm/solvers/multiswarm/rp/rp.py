d4c04b9353bdd97cb3c2e225d2275f319bd33d40
Zander Mao <zma40@sfu.ca>
2025-07-01 19:32:09 -0700

    implement random priority solver

--- src/swarm_prm/solvers/multiswarm/rp/rp.py
+++ src/swarm_prm/solvers/multiswarm/rp/rp.py
@@ -17,23 +17,42 @@ class RandomPriority:
         self.flow_constraints = None
         self.time_limit = time_limit
 
-    def load_instance(self, instance):
-        """
-            Convert instance parameters to a planning instance
-        """
-        
     def plan(self):
         """
             Sequentially plan for each instance.
         """
-        swarms = []
-        solution_found = [False for _ in swarms]
+        solution_found = [False for _ in self.instances]
         start_time = time.time()
-        while time.time() - start_time < self.time_limit:
-            
-            capacity_dicts = []
-            obstacle_goal_dicts = []
-            flow_dicts = []
 
-            for instance in self.instances:
-                pass
+        while time.time() - start_time < self.time_limit:
+            paths = []
+            if np.all(solution_found):
+                print(f"solution found, solution time: {time.time() - start_time}.")
+                return {
+                    "success": True,
+                    "paths": paths
+                }
+
+            order = np.random.permutation(len(self.instances))
+            constraint_dicts = {
+                "capacity_dicts": [],
+                "obstacle_goal_dicts": [],
+                "flow_dicts": [],
+                "max_timestep": 0
+            }
+            for idx in order:
+                print(f"Planning for swarm {idx}")
+                solution = self.instances[idx].solve(**constraint_dicts)
+                if solution["success"]:
+                    constraint_dicts["capacity_dicts"].append(solution["capacity_dict"])
+                    constraint_dicts["obstacle_goal_dicts"].append(solution["goal_state_dict"])
+                    constraint_dicts["flow_dicts"].append(solution["flow_dict"])
+                    constraint_dicts["max_timestep"] = max(constraint_dicts["max_timestep"], solution["timestep"])
+                    # store solution paths
+                    paths.append(solution["paths"])
+                else:
+                    print("No solution found. Randomizing order.")
+                    break
+        print("Timelimit Exceeded.") 
+        return {"success": False}
+                
