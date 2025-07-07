61ab3ac7f47439c1191bcadfb6f405afd24104fd
Zander Mao <zma40@sfu.ca>
2025-07-06 19:26:17 -0700

    update multi-swarm experiment

--- src/st_gaussian_prm/solvers/micro/apf/apf.py
+++ src/st_gaussian_prm/solvers/micro/apf/apf.py
@@ -15,8 +15,8 @@ class APF:
     """
     def __init__(self, gaussian_paths, gaussian_nodes, **apf_config):
         
-        # APF parameters. Decided by experiment
-        self.gaussian_paths = gaussian_paths 
+        # APF parameters. Emperically chosen for experiment
+        self.gaussian_paths = gaussian_paths  
         self.num_agents = len(gaussian_paths)
         self.gaussian_nodes = gaussian_nodes
         self.goal_chisq_threshold= apf_config.get("goal_chisq_threshold", 5.991) # for 95% CI
@@ -29,31 +29,38 @@ class APF:
         self.max_step = apf_config.get("max_step", 0.07)
         self.damping = apf_config.get("damping", 0.7)
         self.max_apf_iterations = apf_config.get("max_apf_iterations", 5000)
-
-    def update(self, t, positions):
+    
+    def update(self, t, positions, fixed_num_steps=1000):
         """
             Update trajectories for one uniform timestep using soft Gaussian
             attractive force.
             Return APF waypoints.
         """
-        assert t+1 < len(self.gaussian_paths[0]), "Gaussian Trajectory index out of bound"
-        goals_gaussians = [self.gaussian_nodes[path[t+1]] for path in self.gaussian_paths]
+        path_len = len(self.gaussian_paths[0])
+        pad_path = False
+        if t+1 < path_len:
+            goals_gaussians = [self.gaussian_nodes[path[t+1]] for path in self.gaussian_paths]
+        else:
+            # Extra padding step
+            goals_gaussians = [self.gaussian_nodes[path[t]] for path in self.gaussian_paths]
+            pad_path = True
         goals_mean = np.array([goals_gaussian.mean for goals_gaussian in goals_gaussians])
         goals_cov = np.array([goals_gaussian.covariance for goals_gaussian in goals_gaussians])
         inv_cov = np.linalg.inv(goals_cov)
+
         trajectories = [] 
-        reached_goal = False
         iter_count = 0
 
         final_positions = np.copy(positions)
-        while not reached_goal:
-            if iter_count > self.max_apf_iterations:
-                raise RuntimeError(f"APF failed to converge: max_iter_num exceeded at timestep {self.max_apf_iterations}") 
+        while True:
             diff = positions - goals_mean
             mahalanobis_sq = np.einsum("ni,nij,nj->n", diff, inv_cov, diff)
-            if np.all(mahalanobis_sq < self.goal_chisq_threshold):
-                reached_goal = True
+            if not pad_path and np.all(mahalanobis_sq < self.goal_chisq_threshold):
                 final_positions = np.copy(positions)
+                break
+            elif pad_path and iter_count == fixed_num_steps:
+                final_positions = np.copy(positions)
+                break
             else: 
                 forces = self.compute_apf_forces(positions, goals_gaussians)
                 step = self.step_size * forces
@@ -62,6 +69,11 @@ class APF:
                 step = step * scaling
                 positions += self.damping*step
                 trajectories.append(np.copy(positions))
+                iter_count += 1
+
+            if iter_count > self.max_apf_iterations:
+                raise RuntimeError(f"APF failed to converge: max_iter_num exceeded at timestep {t}") 
+
         trajectories = np.stack(trajectories, axis=1)
         if trajectories.shape[1] > 1:
             return trajectories[:, 1:, :], final_positions
@@ -104,10 +116,11 @@ class APF:
         positions = np.vstack([np.random.multivariate_normal(mean=g_node.mean, cov=g_node.covariance)
                      for g_node in start_gaussians])
         
-        for t in range(len(self.gaussian_paths[0])-1):
+        for t in range(len(self.gaussian_paths[0])):
             trajectories, final_positions = self.update(t, positions)
             positions = final_positions 
             segments.append(trajectories)
+
         return np.concatenate(segments, axis=1)
 
 if __name__ == "__main__":
